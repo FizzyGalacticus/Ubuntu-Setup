0c3daef2c6f655f7048a50658274edef52f0f93a
root <root@dustin-desktop.(none)>
2014-08-06 08:29:05 -0800

    Added --yes to all install commands to smooth the process.

--- install.sh
+++ install.sh
@@ -5,41 +5,41 @@
 cd ~/Downloads
 
 #Remove Firefox
-sudo apt-get remove --purge firefox &&
-sudo apt-get remove --purge firefox-locale-en &&
+sudo apt-get remove --purge firefox --yes &&
+sudo apt-get remove --purge firefox-locale-en --yes &&
 
 #Remove Thunderbird
-sudo apt-get remove --purge thunderbird && 
-sudo apt-get remove --purge thunderbird-globalmenu && 
-sudo apt-get remove --purge thunderbird-gnome-support && 
-sudo apt-get remove --purge thunderbird-locale-en && 
-sudo apt-get remove --purge thunderbird-locale-en-us &&
+sudo apt-get remove --purge thunderbird --yes && 
+sudo apt-get remove --purge thunderbird-globalmenu --yes && 
+sudo apt-get remove --purge thunderbird-gnome-support --yes && 
+sudo apt-get remove --purge thunderbird-locale-en --yes && 
+sudo apt-get remove --purge thunderbird-locale-en-us --yes &&
 
 #Remove Libre Office
-sudo apt-get remove --purge libreoffice-base-core &&
-sudo apt-get remove --purge libreoffice-core &&
-sudo apt-get remove --purge libreoffice-common &&
-sudo apt-get remove --purge libreoffice-style-human &&
+sudo apt-get remove --purge libreoffice-base-core --yes &&
+sudo apt-get remove --purge libreoffice-core --yes &&
+sudo apt-get remove --purge libreoffice-common --yes &&
+sudo apt-get remove --purge libreoffice-style-human --yes &&
 
 #Disable web advertisements in dash
 wget -q -O - https://fixubuntu.com/fixubuntu.sh | bash &&
 
 #Reset resolvconf (I've had issues connecting to the internet right out of the box)
-sudo apt-get remove --purge resolvconf && 
-sudo apt-get install --reinstall resolvconf &&
+sudo apt-get remove --purge resolvconf --yes && 
+sudo apt-get install --reinstall resolvconf --yes &&
 
 #Enable the universe and multiverse repositories
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe multiverse" &&
 
 #Update packages
-sudo apt-get update &&
+sudo apt-get update --yes &&
 
 #Install Google Chrome dependencies
-sudo apt-get install libxss1 &&
+sudo apt-get install libxss1 --yes &&
 
 #Install Qt dependencies
-sudo apt-get install mesa-common-dev &&
-sudo apt-get install libglu1-mesa-dev &&
+sudo apt-get install mesa-common-dev --yes &&
+sudo apt-get install libglu1-mesa-dev --yes &&
 
 #Install Qt && Google Chrome
 if [[ $(getconf LONG_BIT) = "64" ]]
@@ -63,22 +63,22 @@ else
 
 	echo "Installing Google Chrome" &&
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb &&
-	sudo dpkg -i google-chrome-stable_current_i386.deb &&
+	sudo dpkg -i google-chrome-stable_current_i386.deb --yes &&
 	rm -f google-chrome-stable_current_i386.deb
 fi
 
 #Install Git
-sudo apt-get install git &&
+sudo apt-get install git --yes &&
 
 #Install build-essential package (mostly for GCC)
-sudo apt-get install build-essential &&
+sudo apt-get install build-essential --yes &&
 
 #Install VLC media player
-sudo apt-get install vlc &&
+sudo apt-get install vlc --yes &&
 
 #Install 'foremost' and 'scalpel' forensic tools
-sudo apt-get install foremost &&
-sudo apt-get install scalpel &&
+sudo apt-get install foremost --yes &&
+sudo apt-get install scalpel --yes &&
 
 #Upgrade all packages
-sudo apt-get upgrade
+sudo apt-get upgrade --yes
