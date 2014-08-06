4d72d2b0829dd79a7ee8a11b0a73db52addcba4d
Dustin L Dodson <dustinleedodson@gmail.com>
2014-08-06 13:44:04 -0800

    Added installation of Android Development Tools.

--- install.sh
+++ install.sh
@@ -4,6 +4,18 @@
 
 cd ~/Downloads
 
+#Enable the universe and multiverse repositories
+echo "Adding main/universe/multiverse repositories..." &&
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe multiverse" &&
+
+#Installing aptitude
+#echo "Installing Aptitude..." &&
+#sudo apt-get install aptitude --yes > /dev/null &&
+
+#Update packages
+echo "Updating package database..." &&
+sudo apt-get update --yes > /dev/null &&
+
 #Remove Firefox
 echo "Removing Firefox..." &&
 sudo apt-get remove --purge firefox --yes > /dev/null &&
@@ -33,14 +45,6 @@ echo "Resetting resolvconf..." &&
 sudo apt-get remove --purge resolvconf --yes > /dev/null && 
 sudo apt-get install --reinstall resolvconf --yes > /dev/null &&
 
-#Enable the universe and multiverse repositories
-echo "Adding main/universe/multiverse repositories..." &&
-sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe multiverse" &&
-
-#Update packages
-echo "Updating package database..." &&
-sudo apt-get update --yes > /dev/null &&
-
 #Install Google Chrome dependencies
 echo "Installing Google Chrome dependencies..." &&
 sudo apt-get install libxss1 --yes > /dev/null &&
@@ -50,31 +54,58 @@ echo "Installing Qt dependencies..." &&
 sudo apt-get install mesa-common-dev --yes > /dev/null &&
 sudo apt-get install libglu1-mesa-dev --yes > /dev/null &&
 
+#Install ADT dependencies
+echo "Instsalling ADT Bundle dependencies"
+sudo apt-get install openjdk-7-jdk --yes > /dev/null &&
+
 #Install Qt && Google Chrome
 if [[ $(getconf LONG_BIT) = "64" ]]
 then
-	echo "64bit Detected" > /dev/null &&
-	echo "Installing Qt" > /dev/null &&
+	echo "64bit Detected" &&
+	echo "Installing Qt" &&
 	wget http://download.qt-project.org/official_releases/qt/5.3/5.3.0/qt-opensource-linux-x64-5.3.0.run &&
 	sudo chmod +x qt-opensource-linux-x64-5.3.0.run > /dev/null &&
 	sudo ./qt-opensource-linux-x64-5.3.0.run > /dev/null &&
+	sudo rm qt-opensource-linux-x64-5.3.0.run > /dev/null &&
 
-	echo "Installing Google Chrome" > /dev/null &&
+	echo "Installing Google Chrome" &&
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null &&
 	sudo dpkg -i google-chrome-stable_current_amd64.deb > /dev/null &&
-	rm -f google-chrome-stable_current_amd64.deb > /dev/null &&
+	sudo rm google-chrome-stable_current_amd64.deb > /dev/null &&
+
+	echo "Downloading and installing ADT Bundle" &&
+	sudo apt-get install libgl1-mesa-dev:i386 --yes > /dev/null &&
+	wget http://dl.google.com/android/adt/adt-bundle-linux-x86_64-20140702.zip > /dev/null &&
+	sudo unzip adt-bundle-linux-x86_64-20140702.zip > /dev/null &&
+	sudo mv adt-bundle-linux-x86_64-20140702.zip ADT > /dev/null &&
+	sudo mv ADT /opt/ > /dev/null &&
+	
 else
-	echo "32bit Detected" > /dev/null &&
-	echo "Installing Qt" > /dev/null &&
+	echo "32bit Detected" &&
+	echo "Installing Qt" &&
 	wget http://download.qt-project.org/official_releases/qt/5.3/5.3.0/qt-opensource-linux-x86-5.3.0.run > /dev/null &&
 	sudo chmod +x qt-opensource-linux-x86-5.3.0.run > /dev/null &&
 	sudo ./qt-opensource-linux-x86-5.3.0.run > /dev/null &&
+	sudo rm qt-opensource-linux-x86-5.3.0.run > /dev/null &&
 
 	echo "Installing Google Chrome" > /dev/null &&
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb > /dev/null &&
 	sudo dpkg -i google-chrome-stable_current_i386.deb --yes > /dev/null &&
-	rm -f google-chrome-stable_current_i386.deb > /dev/null &&
+	sudo rm google-chrome-stable_current_i386.deb > /dev/null &&
+
+	echo "Downloading and installing ADT Bundle" &&
+	sudo apt-get install libgl1-mesa-dev --yes > /dev/null &&
+	wget http://dl.google.com/android/adt/adt-bundle-linux-x86-20140702.zip > /dev/null &&
+	sudo unzip adt-bundle-linux-x86-20140702.zip > /dev/null &&
+	sudo mv adt-bundle-linux-x86-20140702.zip ADT > /dev/null &&
+	sudo mv ADT /opt/ > /dev/null &&
 fi
+
+#Add ADT directories to PATH
+PATH=${PATH}:/opt/ADT/sdk/tools &&
+PATH=${PATH}:/opt/ADT/sdk/platform-tools &&
+PATH=${PATH}:/opt/ADT/eclipse &&
+sudo echo "PATH DEFAULT=${PATH}" >> ~/.pam_environment &&
 
 #Install Git
 echo "Installing Git..." &&
