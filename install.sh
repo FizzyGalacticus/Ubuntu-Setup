da72d2098a8f30f597e15e7076cf2a533739a1d4
Dustin L Dodson <dustinleedodson@gmail.com>
2014-08-06 11:09:20 -0800

    Disabled echo for command output and added echo commands to inform of status.

--- install.sh
+++ install.sh
@@ -5,80 +5,98 @@
 cd ~/Downloads
 
 #Remove Firefox
-sudo apt-get remove --purge firefox --yes &&
-sudo apt-get remove --purge firefox-locale-en --yes &&
+echo "Removing Firefox..." &&
+sudo apt-get remove --purge firefox --yes > /dev/null &&
+sudo apt-get remove --purge firefox-locale-en --yes > /dev/null &&
 
 #Remove Thunderbird
-sudo apt-get remove --purge thunderbird --yes && 
-sudo apt-get remove --purge thunderbird-globalmenu --yes && 
-sudo apt-get remove --purge thunderbird-gnome-support --yes && 
-sudo apt-get remove --purge thunderbird-locale-en --yes && 
-sudo apt-get remove --purge thunderbird-locale-en-us --yes &&
+echo "Removing Thunderbird..." &&
+sudo apt-get remove --purge thunderbird --yes > /dev/null && 
+sudo apt-get remove --purge thunderbird-globalmenu --yes > /dev/null && 
+sudo apt-get remove --purge thunderbird-gnome-support --yes > /dev/null && 
+sudo apt-get remove --purge thunderbird-locale-en --yes > /dev/null && 
+sudo apt-get remove --purge thunderbird-locale-en-us --yes > /dev/null &&
 
 #Remove Libre Office
-sudo apt-get remove --purge libreoffice-base-core --yes &&
-sudo apt-get remove --purge libreoffice-core --yes &&
-sudo apt-get remove --purge libreoffice-common --yes &&
-sudo apt-get remove --purge libreoffice-style-human --yes &&
+echo "Removing Libre Office..." &&
+sudo apt-get remove --purge libreoffice-base-core --yes > /dev/null &&
+sudo apt-get remove --purge libreoffice-core --yes > /dev/null &&
+sudo apt-get remove --purge libreoffice-common --yes > /dev/null &&
+sudo apt-get remove --purge libreoffice-style-human --yes > /dev/null &&
 
 #Disable web advertisements in dash
-wget -q -O - https://fixubuntu.com/fixubuntu.sh | bash &&
+echo "Disabling web advertizements in dash..." &&
+wget -q -O - https://fixubuntu.com/fixubuntu.sh | bash > /dev/null &&
 
 #Reset resolvconf (I've had issues connecting to the internet right out of the box)
-sudo apt-get remove --purge resolvconf --yes && 
-sudo apt-get install --reinstall resolvconf --yes &&
+echo "Resetting resolvconf..." &&
+sudo apt-get remove --purge resolvconf --yes > /dev/null && 
+sudo apt-get install --reinstall resolvconf --yes > /dev/null &&
 
 #Enable the universe and multiverse repositories
+echo "Adding main/universe/multiverse repositories..." &&
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe multiverse" &&
 
 #Update packages
-sudo apt-get update --yes &&
+echo "Updating package database..." &&
+sudo apt-get update --yes > /dev/null &&
 
 #Install Google Chrome dependencies
-sudo apt-get install libxss1 --yes &&
+echo "Installing Google Chrome dependencies..." &&
+sudo apt-get install libxss1 --yes > /dev/null &&
 
 #Install Qt dependencies
-sudo apt-get install mesa-common-dev --yes &&
-sudo apt-get install libglu1-mesa-dev --yes &&
+echo "Installing Qt dependencies..." &&
+sudo apt-get install mesa-common-dev --yes > /dev/null &&
+sudo apt-get install libglu1-mesa-dev --yes > /dev/null &&
 
 #Install Qt && Google Chrome
 if [[ $(getconf LONG_BIT) = "64" ]]
 then
-	echo "64bit Detected" &&
-	echo "Installing Qt" &&
+	echo "64bit Detected" > /dev/null &&
+	echo "Installing Qt" > /dev/null &&
 	wget http://download.qt-project.org/official_releases/qt/5.3/5.3.0/qt-opensource-linux-x64-5.3.0.run &&
-	sudo chmod +x qt-opensource-linux-x64-5.3.0.run &&
-	sudo ./qt-opensource-linux-x64-5.3.0.run &&
+	sudo chmod +x qt-opensource-linux-x64-5.3.0.run > /dev/null &&
+	sudo ./qt-opensource-linux-x64-5.3.0.run > /dev/null &&
 
-	echo "Installing Google Chrome" &&
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
-	sudo dpkg -i google-chrome-stable_current_amd64.deb &&
-	rm -f google-chrome-stable_current_amd64.deb
+	echo "Installing Google Chrome" > /dev/null &&
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null &&
+	sudo dpkg -i google-chrome-stable_current_amd64.deb > /dev/null &&
+	rm -f google-chrome-stable_current_amd64.deb > /dev/null &&
 else
-	echo "32bit Detected" &&
-	echo "Installing Qt" &&
-	wget http://download.qt-project.org/official_releases/qt/5.3/5.3.0/qt-opensource-linux-x86-5.3.0.run &&
-	sudo chmod +x qt-opensource-linux-x86-5.3.0.run &&
-	sudo ./qt-opensource-linux-x86-5.3.0.run &&
+	echo "32bit Detected" > /dev/null &&
+	echo "Installing Qt" > /dev/null &&
+	wget http://download.qt-project.org/official_releases/qt/5.3/5.3.0/qt-opensource-linux-x86-5.3.0.run > /dev/null &&
+	sudo chmod +x qt-opensource-linux-x86-5.3.0.run > /dev/null &&
+	sudo ./qt-opensource-linux-x86-5.3.0.run > /dev/null &&
 
-	echo "Installing Google Chrome" &&
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb &&
-	sudo dpkg -i google-chrome-stable_current_i386.deb --yes &&
-	rm -f google-chrome-stable_current_i386.deb
+	echo "Installing Google Chrome" > /dev/null &&
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb > /dev/null &&
+	sudo dpkg -i google-chrome-stable_current_i386.deb --yes > /dev/null &&
+	rm -f google-chrome-stable_current_i386.deb > /dev/null &&
 fi
 
 #Install Git
-sudo apt-get install git --yes &&
+echo "Installing Git..." &&
+sudo apt-get install git --yes > /dev/null &&
 
 #Install build-essential package (mostly for GCC)
-sudo apt-get install build-essential --yes &&
+echo "Installing build-essential package..." &&
+sudo apt-get install build-essential --yes > /dev/null &&
 
 #Install VLC media player
-sudo apt-get install vlc --yes &&
+echo "Installing VLC Media Player..." &&
+sudo apt-get install vlc --yes > /dev/null &&
 
 #Install 'foremost' and 'scalpel' forensic tools
-sudo apt-get install foremost --yes &&
-sudo apt-get install scalpel --yes &&
+echo "Installing Foremost and Scalpel..." &&
+sudo apt-get install foremost --yes > /dev/null &&
+sudo apt-get install scalpel --yes > /dev/null &&
+
+#Remove unecessary packages
+echo "Removing unecessary packages..." &&
+sudo apt-get autoremove --yes > /dev/null &&
 
 #Upgrade all packages
-sudo apt-get upgrade --yes
+echo "Upgrading installed packages..." &&
+sudo apt-get upgrade --yes > /dev/null 
